70d9efdbcdb96a31dfc0feb5e90d1f83f6570b98
Ralf Schmitt <ralf@systemexit.de>
2019-02-18 23:26:30 +0100

    add command line parsing
    allows specifying the location of the config file.
    
    Automatically ignore the .soliumignore file, since pre-commit takes care
    of that.

--- src/ethlint_pre_commit/core.cljs
+++ src/ethlint_pre_commit/core.cljs
@@ -1,22 +1,51 @@
 (ns ethlint-pre-commit.core
-  (:require ["ethlint/lib/cli" :as cli]))
+  (:require ["ethlint/lib/cli" :as cli]
+            [clojure.tools.cli :refer [parse-opts]]
+            ))
 
 (set! *warn-on-infer* true)
+
 
 (defn exit
   "Exit with the given status."
   [status]
   (.exit js/process status))
 
+
+(defn run-solium
+  [{:keys [config]} args]
+  (when-not (empty? args)
+    (let [file-args (interleave (repeat "-f")
+                                args)
+          cmd (apply vector "node" "ethlint"
+                     "--no-soliumignore"
+                     "--config" config
+                     file-args)]
+      (cli/execute (clj->js cmd)))))
+
+
+(defn show-usage
+  [summary]
+  (println (str
+            "Usage: ethlint-pre-commit [options] SOLIDITY_FILE ..." \newline
+            "Options:" \newline
+            summary)))
+
+
+(def cli-options
+  [["-c" "--config CONFIG" "path to .soliumrc file" :default ".soliumrc"]
+   ["-h" "--help" "show help"]])
+
+
 (defn -main
   [& args]
-
-  (let [file-args (interleave (repeat "-f")
-                              args)
-        cmd (apply vector "ethlint" file-args)]
+  (let [{:keys [options arguments summary errors]} (parse-opts args cli-options)]
     (try
-      (when-not (empty? file-args)
-        (cli/execute (clj->js cmd)))
+      (cond
+        (:help options)
+          (show-usage summary)
+        :else
+          (run-solium options arguments))
       (exit 0)
       (finally
         (exit 1)))))
